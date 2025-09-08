37ed4c62beed33e5c69cec4ecac9e95cd730e4e6
Daniel Olivera <olivera.dano@icloud.com>
2025-09-08 16:06:24 +0000

    luces

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "/workspaces/React-Semaforo-Danonino/src/styles/index.css"
 
-//Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+function TrafficLight() {
+  const [color, setColor] = useState("red");
 
-// index.css'
-import '../styles/index.css'
+  return (
+    <div className="traffic-light">
+      <div className="traffic-box">
+        <div
+          onClick={() => setColor("red")}
+          className={`light red ${color === "red" ? "on" : "none"}`}
+        />
+        <div
+          onClick={() => setColor("yellow")}
+          className={`light yellow ${color === "yellow" ? "on" : "none"}`}
+        />
+        <div
+          onClick={() => setColor("green")}
+          className={`light green ${color === "green" ? "on" : "none"}`}
+        />
+      </div>
+    </div>
+  );
+}
 
-// components
-import Home from './components/Home';
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<TrafficLight />);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+export default TrafficLight;
